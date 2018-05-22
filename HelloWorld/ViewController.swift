ab1acce4d061fe68b006ddddb8e06b4414d8b79b
Ameya Gholkar <ameyagh@microsoft.com>
2018-05-22 10:47:54 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ameya Gholkar on 5/22/18.
+//  Copyright © 2018 Ameya Gholkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
